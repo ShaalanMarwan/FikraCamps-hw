b5194342d055f22612bd030b2ccaf8bebad28d3b
Ruqayah22 <ruqayah.alnawfaly@outlook.com>
2021-09-13 12:09:40 +0300

    Create Q2.js

---
+++ Q2.js
@@ -0,0 +1,20 @@
+/** Q2
+ * assume you have two time periods
+ * for example
+ * period1 = 13/5/2021 01:00 to 14/5/2021 01:00
+ * period2 = 15/5/2021 01:00 to 16/5/2021 01:00
+ * write a function that tells us whether two given periods overlap or not
+ * example CheckOverlap("13/5/2021 13:00","14/5/2021 13:00", "15/5/2021 13:00","16/5/2021 13:00" ) => no overlap
+ * example CheckOverlap("13/5/2021 13:00","14/5/2021 13:00", "14/5/2021 13:00","16/5/2021 13:00" ) => overlap
+ */
+
+ function CheckOverlap(t1Start, t1End, t2Start, t2End) {}
+ console.log(
+   CheckOverlap(
+     "13/5/2021 13:00",
+     "14/5/2021 13:00",
+     "14/5/2021 13:00",
+     "16/5/2021 13:00"
+   )
+ );
+ 
