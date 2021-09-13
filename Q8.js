3f947d2594a3e7b6dd5634b6d41d9e2960d3ae25
Ruqayah22 <ruqayah.alnawfaly@outlook.com>
2021-09-13 12:10:22 +0300

    Create Q8.js

---
+++ Q8.js
@@ -0,0 +1,16 @@
+/** Q8
+  * Write a function that accepts a positive integer 
+  * between 0 and 999 inclusive and returns a string representation of that integer written in English.
+ 
+ Examples
+ numToEng(0) ➞ "zero"
+ 
+ numToEng(18) ➞ "eighteen"
+ 
+ numToEng(126) ➞ "one hundred twenty six"
+ 
+ numToEng(909) ➞ "nine hundred nine"
+ Notes
+ There are no hyphens used (e.g. "thirty five" not "thirty-five").
+ The word "and" is not used (e.g. "one hundred one" not "one hundred and one").
+  */
